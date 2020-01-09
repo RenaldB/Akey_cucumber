fdf6781775ec9ad514d6f0807f3ec9ff1785cf6d
renald.booz <renald.booz@PC-05-FLAMBOYANT.FORMATION.LOCAL>
2020-01-09 13:20:07 +0400

    steps update

--- akey_cucumber_project/src/test/java/dojo/CocktailSteps.java
+++ akey_cucumber_project/src/test/java/dojo/CocktailSteps.java
@@ -12,16 +12,16 @@ public class CocktailSteps {
 
 	private Order order;
 	
-	@Given("Romeo who wants to buy a drink")
-	public void romeo_who_wants_to_buy_a_drink() {
-		 order = new Order();
-	     order.declareOwner("Romeo");
+	@Given("(.*) who wants to buy a drink")  
+	public void romeo_who_wants_to_buy_a_drink(String romeo) {  
+		order = new Order();  
+		order.declareOwner(romeo);  
 	}
 
-	@When("an order is declared for Juliette")  
-   	public void an_order_is_declared_for_juliette() {  
-    	order.declareTarget("Juliette");  
-	} 
+	@When("an order is declared for (.*)")  
+	public void an_order_is_declared_for_Juliette(String juliette) {  
+		order.declareTarget(juliette);  
+	}
 
 	@Then("there is no cocktail in the order")
 	public void there_is_no_cocktail_in_the_order() {
