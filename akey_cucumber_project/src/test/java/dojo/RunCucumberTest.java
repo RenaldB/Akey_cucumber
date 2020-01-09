7c6170a48adf7270c02b27013b02428728c98ab2
renald.booz <renald.booz@FLAMBOYANT-07.FORMATION.LOCAL>
2020-01-09 13:11:23 +0400

    Run and order

--- akey_cucumber_project/src/test/java/dojo/RunCucumberTest.java
+++ akey_cucumber_project/src/test/java/dojo/RunCucumberTest.java
@@ -1,9 +1,11 @@
 package dojo;  
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;  
 import org.junit.runner.RunWith;  
 
 @RunWith(Cucumber.class)  
+@CucumberOptions(strict = true)
 public class RunCucumberTest { 
 	
 }
